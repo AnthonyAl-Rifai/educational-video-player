5ad2cefedbea737eee47f74af064103fbf6a34ef
AnthonyAl-Rifai <anthonyalrifai@gmail.com>
2025-09-01 13:06:35 -0700

    add HeroSection to home.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import HeroSection from '@/components/HeroSection';
+
 export default function Home() {
-  return <div>hello</div>;
+  return (
+    <main className="min-h-screen max-w-[1504px] mx-auto px-4 sm:px-6 lg:px-8 mt-20">
+      <HeroSection />
+      <section className="bg-amber-200 h-[100vh]"></section>
+    </main>
+  );
 }
